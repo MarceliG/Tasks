4ad0a685dd217cb2419dd01563a65e890676e151
MarceliG <marcel9817@gmail.com>
2022-10-18 14:49:34 +0200

    add function compare words from list

--- task1/translaor.py
+++ task1/translaor.py
@@ -50,14 +50,26 @@ def give_only_tranlated_words(page):
     return elements[17:-1]
 
 
+def compare_all_translated_word_to_str(translated_words: list):
+    """marge all element from list to string
+
+    Args:
+        translated_words (list): list with translated words
+
+    Returns:
+        str: string with translated word
+    """
+    result_as_string = ""
+    for i in translated_words:
+        result_as_string += i + " "
+    return result_as_string
+
+
 def translate(word: str = "") -> str:
     encoded_word = encoding(word)
     page = send_request(encoded_word)
-    translated = give_only_tranlated_words(page)
-
-    result = ""
-    for i in translated:
-        result += i + " "
+    translated_words = give_only_tranlated_words(page)
+    result = compare_all_translated_word_to_str(translated_words)
 
     print(result)
 
