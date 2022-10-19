a3cea898d4ac503d3afd34df562485fd24432cf0
MarceliG <marcel9817@gmail.com>
2022-10-19 23:41:28 +0200

    translate few words

--- task1/translaor.py
+++ task1/translaor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import argparse
+from operator import index
 import requests
 from requests.utils import quote
 from bs4 import BeautifulSoup
@@ -7,17 +8,24 @@ from typing import Optional
 from typing import Sequence
 
 
-def encoding(word: str):
+def encoding(words: list):
     """Encoding any word.
 
     Args:
-        word (str): the word.
+        word (list): the word.
 
     Returns:
-        (str): Encoded word.
+        (str): Encoded word/words.
     """
+
     try:
-        return quote(word, safe="")
+        result = ""
+        for word in words:
+            result += quote(word, safe="")
+            if words.index(word) < len(words) and words.index(word) > 0:
+                result += "+"
+        print(result)
+        return result
     except:
         return ""
 
@@ -65,7 +73,7 @@ def compare_all_translated_word_to_str(translated_words: list):
     return result_as_string
 
 
-def translate(word: str = "") -> str:
+def translate(word: list) -> str:
     encoded_word = encoding(word)
     page = send_request(encoded_word)
     translated_words = give_only_tranlated_words(page)
@@ -76,8 +84,11 @@ def translate(word: str = "") -> str:
 
 def main(argument: Optional[Sequence[str]] = None):
     parser = argparse.ArgumentParser(description="tranlate any word")
-    parser.add_argument("word", help="Enter a word to translate")
+    parser.add_argument(
+        "word", type=str, nargs="+", help="Enter a word to translate"
+    )
     args = parser.parse_args()
+    print(args.word)
     translate(args.word)
     return 0
 
