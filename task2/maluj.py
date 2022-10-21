f75cdc6999363d4d0fa86d52b781000a8297eb6b
MarceliG <marcel9817@gmail.com>
2022-10-21 22:28:22 +0200

    create class Read file

--- task2/maluj.py
+++ task2/maluj.py
@@ -1,8 +1,40 @@
-import PIL
+class Read:
+    def __init__(self):
+        """canvas [W] [H]
+        triangle [color] [point A], [point B], [point C]
+        point -> (x, y)
+        rectangle [color] [point] [size]
+        size -> (w, h)
+        """
+        self.properties_to_draw = {
+            "canvas": [],
+            "triangle": [],
+            "rectangle": [],
+        }
+
+    def _give_pure_line(self, line: str):
+        return [x.strip(" (),") for x in line.split()[1:]]
+
+    def read_file(self, file_name):
+        with open(file_name) as f:
+            for line in f.readlines():
+                if "canvas" in line:
+                    self.properties_to_draw["canvas"] = self._give_pure_line(
+                        line
+                    )
+                elif "triangle" in line:
+                    self.properties_to_draw["triangle"] = self._give_pure_line(
+                        line
+                    )
+                elif "rectangle" in line:
+                    self.properties_to_draw[
+                        "rectangle"
+                    ] = self._give_pure_line(line)
 
 
 def main():
-    pass
+    draw = Read()
+    draw.read_file()
 
 
 if __name__ == "__main__":
